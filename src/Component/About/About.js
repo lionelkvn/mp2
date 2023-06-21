fdac2b9dcf631e15d418dfa8e218f74e6a69ef1a
JohnMichaelMWong <johnmichaelmwong0411@gmail.com>
2023-06-21 09:05:30 +0800

    21062023

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -2,16 +2,12 @@ import './About.css';
 const About = () => {
     return(
         <div id="About" class="About">
-            <div class="AboutImage">
-                <img src="./AboutImage.jpg"/>
-            </div>
             <div class='AboutContent'>
                 <h1><span class="secondary-color">About </span><span>Us</span></h1> 
-                <h3>a graphic design studio specializing in branding, branded illustration systems, and packaging.
-                     Our goal is to create meaningful and timeless design for our clients and their audience.</h3>
-                <h3>We believe that great work comes from building great relationships. We work one on one with our
-                    clients and collaborate with them every step of the way.</h3>
-                <h3><span>Our </span><span class="secondary-color">GOAL</span></h3><h3> is to inspire and attract the target audience.</h3>
+                <h2>a graphic design studio specializing in branding, branded illustration systems, and packaging.
+                     Our goal is to create meaningful and timeless design for our clients and their audience.</h2>
+                <h2>We believe that great work comes from building great relationships. We work one on one with our
+                    clients and collaborate with them every step of the way.</h2>
             </div>
         </div>
     );
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -2,14 +2,14 @@ import './Home.css';
 const Home = () => {
     return(
         <div id="Home" class="Home">
+            <div class="HomeImage">
+                <img src="./slide image.png"/>
+            </div>
             <div class='HomeContent'>
                 <h2>WE DO AWESOME THINGS</h2>
                 <h1><span class="primary-color">Ink</span><span>novative</span></h1> 
                 <h2>Putting your thoughts into prints.</h2>
                 <div><a href="#" class="button">Contact Us!</a></div>
-            </div>
-            <div class="HomeImage">
-                <img src="./slide image.png"/>
             </div>
         </div>
     );
