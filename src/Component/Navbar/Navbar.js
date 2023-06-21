3929ead9b8164c0a9be7328fb15553de4a2d019e
JohnMichaelMWong <johnmichaelmwong0411@gmail.com>
2023-06-21 16:55:01 +0800

    21062023

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -3,7 +3,8 @@ const Navbar = () => {
     return(
         <div id="Navbar" className="container">
             <div className="Navbar">
-            <h1><span class="primary-color">Ink</span><span>novative</span></h1>          
+            <h1><span class="primary-color">Ink</span><span>novative</span></h1>   
+            <div><a href="#" class="button">Contact Us!</a></div>       
             </div>
             <div className="Navbar">
                 <a href="#" className="active"><span>Home</span></a>
