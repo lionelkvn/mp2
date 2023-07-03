92419772a370d705f55c954ad2cfffb70cef9910
lionelkvn <lionel.natividad@gmail.com>
2023-07-03 10:52:52 +0800

    added feature pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import './App.css';
 import React from 'react';
 import "@fontsource/poppins";
-import Navbar from './Component/Navbar/Navbar';
-import Home from './Component/Home/Home';
-import About from './Component/About/About';
-import Contact from './Component/Contact/Contact';
-import Portfolio from './Component/Portfolio/Portfolio';
-import Service from './Component/Services/Service';
-import ServicesOfferLogo from './Component/ServicesOfferLogo/ServicesOfferLogo';
-import ServicesOfferBrochure1 from './Component/ServiceOfferBrochure1/ServiceOfferBrochure1';
+import {Routes, Route } from "react-router-dom";
+import Homepage from './Pages/Homepage';
+import PurchasePage from './Pages/PurchasePage';
+import PurchasePageBrochure from "./Pages/PurchasePageBrochure"
+
 function Apps() {
   return (
-    <div>
-      <Navbar />
-      <Home />
-      <About />
-      <Service />
-      <ServicesOfferLogo />
-      <ServicesOfferBrochure1 />
-      <Portfolio />
-      <Contact />
-    </div>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/purchase" element={<PurchasePage />} />
+      <Route path="/purchasebrochure" element={<PurchasePageBrochure />} />
+    </Routes>
   );
 }
 
